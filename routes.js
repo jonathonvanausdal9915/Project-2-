e4e3d1afdcd3961c28ebf3939bccf450d141c38c
Sejin <kisejin2015@gmail.com>
2022-09-22 20:48:13 -0600

    routes

--- routes.js
+++ routes.js
@@ -7,3 +7,18 @@ app.get("", function (req, res) {
     db = JSON.parse(fs.readFileSync("./db/db.json")) || []
     res.json(db)
 });
+
+app.post("", function (req, res) {
+    let noName = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(Math.random() * 9451)
+    }
+    db.push(noName)
+    fs.writeFileSync("file path", JSON.stringify(db), function (err) {
+        if (err) throw err
+    })
+    res.json(db)
+});
+
+module.exports = app;
