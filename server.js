47d10a46ab2eecf7028fc399e8827703e66cfe3f
Zach <z.d.jorgensen@gmail.com>
2022-09-26 18:33:35 -0600

    lots of small things

--- server.js
+++ server.js
@@ -30,11 +30,12 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(routes);
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`)
