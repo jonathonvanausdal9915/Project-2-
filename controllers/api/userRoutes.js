ea033d584376a0af52f25f5af959ed2a6d70f5b8
Jonathon <jonathonvanausdal257@gmail.com>
2022-09-27 12:37:31 -0600

    Changes to routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -13,3 +13,4 @@ router.post('/', async(req, res) => {
         res.status(400).json(err);
     }
 });
+module.exports = router;
--- controllers/home.routes.js
+++ controllers/home.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Workout, User } = require('../models');
 
-router.get('/', async (req, res) => {
+router.get('/', async(req, res) => {
     try {
         // Get all users, sorted by name
         const userData = await User.findAll({
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
             ],
         });
 
-    const workout = workoutData.map((workout) => workout.get({ plain: true }));
+        const workout = workoutData.map((workout) => workout.get({ plain: true }));
 
         // Pass serialized data and session flag into template
         res.render('homepage', {
@@ -23,25 +23,23 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/workout/:id', async (req, res) => {
+router.get('/workout/:id', async(req, res) => {
     try {
-      const projectData = await Project.findByPk(req.params.id, {
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
-      });
+        const projectData = await Project.findByPk(req.params.id, {
+            include: [{
+                model: User,
+                attributes: ['name'],
+            }, ],
+        });
 
-// Serialize user data so templates can read it
-const users = userData.map((project) => project.get({ plain: true }));
+        // Serialize user data so templates can read it
+        const users = userData.map((project) => project.get({ plain: true }));
 
-// Pass serialized data into Handlebars.js template
-res.render('homepage', { users });
+        // Pass serialized data into Handlebars.js template
+        res.render('homepage', { users });
     } catch (err) {
-    res.status(500).json(err);
-}
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
