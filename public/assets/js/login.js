81c5ae35cfb45c54708a26a8eaeef6254540657c
Zach <z.d.jorgensen@gmail.com>
2022-09-27 23:24:41 -0600

    routes still broken

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,11 +1,10 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const email = document.querySelector('#user-email').value.trim();
+    const password = document.querySelector('#user-password').value.trim();
   
     if (email && password) {
-      // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
@@ -13,7 +12,6 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        // If successful, redirect the browser to the profile page
         document.location.replace('/homepage');
       } else {
         alert(response.statusText);
@@ -21,33 +19,4 @@ const loginFormHandler = async (event) => {
     }
   };
   
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/homepage');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
-  
+  document.querySelector('#signupbtn').addEventListener('submit', loginFormHandler);
--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 // Checks to see if user is logged in or not
 const Auth = (req, res, next) => {
     if (!req.session.logged_in) {
-      res.redirect('/login');
+      res.redirect('/');
     } else {
       next();
     }
