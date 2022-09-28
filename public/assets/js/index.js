66fc19510edfc79c9bdaf73cb735b792435a64b4
Jonathon <jonathonvanausdal257@gmail.com>
2022-09-27 21:13:55 -0600

    button goes to signup html on click

--- public/assets/js/index.js
+++
@@ -1,6 +0,0 @@
-let signUpBtn = document.getElementById("signupbtn");
-
-signUpBtn.onclick = function() {
-    document.location.replace("../signup.html")
-};
-//Replace onclick href with the route of the signup page.
