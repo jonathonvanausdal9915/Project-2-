0e86baa4cb41d7d8b0e2a268123dd724eddda5fe
Zach <z.d.jorgensen@gmail.com>
2022-09-27 23:56:32 -0600

    profile.js and profile.handlebars

---
+++ public/assets/js/profile.js
@@ -0,0 +1,25 @@
+const displayProfile = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#first-name').value.trim();
+    const lastName = document.querySelector('#last-name').value.trim();
+    const email = document.querySelector('#email').value.trim();
+
+    if(name && lastName && email) {
+        const response = await fetch(`/api/users`, {
+            method: 'POST',
+            body:  JSON.stringify({ name, lastName, email }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            document.location.replace('/');
+        }
+    }
+};
+
+document.querySelector('#saveBtn').addEventListener('submit', displayProfile);
