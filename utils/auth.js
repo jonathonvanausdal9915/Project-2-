04df9666241bc3e2cfe21d294d3fdf587fa1090b
Zach <z.d.jorgensen@gmail.com>
2022-09-28 23:01:26 -0600

    profile style/route

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,5 @@
 // Checks to see if user is logged in or not
-const Auth = (req, res, next) => {
+const withAuth = (req, res, next) => {
     if (!req.session.logged_in) {
       res.redirect('/');
     } else {
@@ -7,4 +7,4 @@ const Auth = (req, res, next) => {
     }
   };
   
-  module.exports = Auth;
+  module.exports = withAuth;
