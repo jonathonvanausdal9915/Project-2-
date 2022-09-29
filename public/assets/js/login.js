711e12ea40aa3025d0734ab5fa48f3a47b7f7c4d
Zach <z.d.jorgensen@gmail.com>
2022-09-28 21:57:40 -0600

    loginBtn working, route not working

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,12 +1,11 @@
 const loginHandler = async(event) => {
-    event.preventDefault();
     console.log('working');
 
     const email = document.querySelector('#email').value.trim();
     const password = document.querySelector('#password').value.trim();
 
     if (email && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
             headers: { 'Content-Type': 'application/json' },
