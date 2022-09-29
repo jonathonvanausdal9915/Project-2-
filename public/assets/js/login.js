1dd64b445ca75e12187ddf82e218314006dd2c3e
Jonathon <jonathonvanausdal257@gmail.com>
2022-09-28 20:47:20 -0600

    signup route and button

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,22 +1,22 @@
-const loginFormHandler = async (event) => {
+const loginFormHandler = async(event) => {
     event.preventDefault();
-  
-    const email = document.querySelector('#user-email').value.trim();
-    const password = document.querySelector('#user-password').value.trim();
-  
+
+    const email = document.querySelector('#user-name').value.trim();
+    const password = document.querySelector('#user_password').value.trim();
+
     if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/homepage');
-      } else {
-        alert(response.statusText);
-      }
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
     }
-  };
-  
-  document.querySelector('#signupbtn').addEventListener('submit', loginFormHandler);
+};
+
+document.querySelector('#siginbtn').addEventListener('submit', loginFormHandler);
