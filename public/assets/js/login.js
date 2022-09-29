bdcbbb04b0a44f09de1716c0a66272676f87f5fa
Zach <z.d.jorgensen@gmail.com>
2022-09-28 20:46:58 -0600

    routes/js

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,22 +1,23 @@
-const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#user-email').value.trim();
-    const password = document.querySelector('#user-password').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/homepage');
-      } else {
-        alert(response.statusText);
-      }
+const loginHandler = async (event) => {
+  event.preventDefault();
+  console.log('working'); 
+
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  document.querySelector('#signupbtn').addEventListener('submit', loginFormHandler);
+  }
+};
+
+document.querySelector('.help').addEventListener('submit', loginHandler);
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -2,7 +2,7 @@ const sequelize = require('../config/connection');
 const { User, Workout } = require('../models');
 
 const userData = require('./userData.json');
-const workoutData = require('./workoutData.json');
+// const workoutData = require('./workoutData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,12 +12,12 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const workout of workoutData) {
-    await workout.create({
-      ...workout,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  // for (const workout of workoutData) {
+  //   await workout.create({
+  //     ...workout,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
 
   process.exit(0);
 };
